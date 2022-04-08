f30a2a7f3aa278e973659bfa71bfb103126884c3
Márton Borsos <borsos.marton95@gmail.com>
2022-04-08 09:52:39 +0200

    Code improvements

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -1,10 +1,12 @@
-from brownie import AdvancedCollectible, network
-from scripts.helpful_scripts import get_breed
-from metadata.sample_metadata import metadata_template
-from pathlib import Path
-import requests
 import json
+from lib2to3.pgen2 import token
 import os
+from pathlib import Path
+from brownie import AdvancedCollectible, network
+from metadata.sample_metadata import metadata_template
+import requests
+
+from scripts.helpful_scripts import get_breed
 
 breed_to_image_uri = {
     "PUG": "https://ipfs.io/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8?filename=pug.png",
@@ -19,31 +21,26 @@ def main():
     print(f"You have created {number_of_advanced_collectibles} collectibles!")
     for token_id in range(number_of_advanced_collectibles):
         breed = get_breed(advanced_collectible.tokenIdToBreed(token_id))
-        metadata_file_name = (
-            f"./metadata/{network.show_active()}/{token_id}-{breed}.json"
-        )
+        metadata_file_name = f"./metadata/{network.show_active()}/{token_id}-{breed}.json"
         collectible_metadata = metadata_template
         if Path(metadata_file_name).exists():
-            print(f"{metadata_file_name} already exists! Delete it to overwrite")
+            print(f"{metadata_file_name} already exists! Delete to overwrite")
         else:
             print(f"Creating Metadata file: {metadata_file_name}")
             collectible_metadata["name"] = breed
             collectible_metadata["description"] = f"An adorable {breed} pup!"
-            image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
+            image_path = f"./img/{breed.lower().replace('_','-')}.png"
 
             image_uri = None
             if os.getenv("UPLOAD_IPFS") == "true":
                 image_uri = upload_to_ipfs(image_path)
             image_uri = image_uri if image_uri else breed_to_image_uri[breed]
-
+            
             collectible_metadata["image"] = image_uri
             with open(metadata_file_name, "w") as file:
                 json.dump(collectible_metadata, file)
             if os.getenv("UPLOAD_IPFS") == "true":
                 upload_to_ipfs(metadata_file_name)
-
-
-# curl -X POST -F file=@metadata/rinkeby/0-SHIBA_INU.json http://localhost:5001/api/v0/add
 
 
 def upload_to_ipfs(filepath):
@@ -53,8 +50,7 @@ def upload_to_ipfs(filepath):
         endpoint = "/api/v0/add"
         response = requests.post(ipfs_url + endpoint, files={"file": image_binary})
         ipfs_hash = response.json()["Hash"]
-        # "./img/0-PUG.png" -> "0-PUG.png"
-        filename = filepath.split("/")[-1:][0]
+        filename = filepath.split("/")[-1]
         image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
         print(image_uri)
         return image_uri
