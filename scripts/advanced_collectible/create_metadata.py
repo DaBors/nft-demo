5114459a13374f36926b0e92bf786d227cba5a2c
Márton Borsos <borsos.marton95@gmail.com>
2022-04-08 16:17:19 +0200

    Added code to upload animation url to ipfs

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -1,5 +1,4 @@
 import json
-from lib2to3.pgen2 import token
 import os
 from pathlib import Path
 from brownie import AdvancedCollectible, network
@@ -12,6 +11,12 @@ breed_to_image_uri = {
     "PUG": "https://ipfs.io/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8?filename=pug.png",
     "SHIBA_INU": "https://ipfs.io/ipfs/QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU?filename=shiba-inu.png",
     "ST_BERNARD": "https://ipfs.io/ipfs/QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW?filename=st-bernard.png",
+}
+
+breed_to_animation_uri = {
+    "PUG": "https://ipfs.io/ipfs/QmQKJGYaRunwnBWHLyG2ZC2tSkaFRbdo7wMxidGwrxoS5u?filename=pug.html",
+    "SHIBA_INU": "https://ipfs.io/ipfs/QmZMhTDssSf7Et7Z3BLii8m8TUZe1uXidVuBpVsUGMLJHR?filename=shiba-inu.html",
+    "ST_BERNARD": "https://ipfs.io/ipfs/QmesstDKshuTFZJjTaiNkMtLrCGiVgsTa97AuW8aWdoS1T?filename=st-bernard.html",
 }
 
 
@@ -29,14 +34,21 @@ def main():
             print(f"Creating Metadata file: {metadata_file_name}")
             collectible_metadata["name"] = breed
             collectible_metadata["description"] = f"An adorable {breed} pup!"
+            
             image_path = f"./img/{breed.lower().replace('_','-')}.png"
-
             image_uri = None
             if os.getenv("UPLOAD_IPFS") == "true":
                 image_uri = upload_to_ipfs(image_path)
             image_uri = image_uri if image_uri else breed_to_image_uri[breed]
-            
             collectible_metadata["image"] = image_uri
+            
+            animation_path = f"./img/{breed.lower().replace('_','-')}.html"
+            animation_url = None
+            if os.getenv("UPLOAD_IPFS") == "true":
+                animation_url = upload_to_ipfs(animation_path)
+            animation_url = animation_url if animation_url else breed_to_animation_uri[breed]
+            collectible_metadata["animation_url"] = animation_url
+            
             with open(metadata_file_name, "w") as file:
                 json.dump(collectible_metadata, file)
             if os.getenv("UPLOAD_IPFS") == "true":
